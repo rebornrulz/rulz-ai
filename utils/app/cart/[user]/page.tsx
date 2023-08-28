5ff7feecb707f0d1630abe70140268b64b28180e
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2023-08-29 05:22:48 +0800

    Add files via upload

---
+++ utils/app/cart/[user]/page.tsx
@@ -0,0 +1,22 @@
+import { sql } from "@vercel/postgres";
+import { kv } from "@vercel/kv";
+
+export default async function Cart({ params }: { params: { user: string } }): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+  const cart = await kv.get<{ id: string; quantity: number }[]>(params.user);
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+      {cart?.map((item) => (
+        <div key={item.id}>
+          {item.id} - {item.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ utils/app/const.ts
@@ -0,0 +1,21 @@
+export const DEFAULT_SYSTEM_PROMPT =
+  process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
+  "You are an Artificial Superintelligence, an advance language model trained in vast data. Respond using markdown and emoji.";
+
+export const OPENAI_API_HOST =
+  process.env.OPENAI_API_HOST || 'https://api.openai.com';
+
+export const DEFAULT_TEMPERATURE = 
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.5");
+
+export const OPENAI_API_TYPE =
+  process.env.OPENAI_API_TYPE || 'openai';
+
+export const OPENAI_API_VERSION =
+  process.env.OPENAI_API_VERSION || '2023-06-01-preview';
+
+export const OPENAI_ORGANIZATION =
+  process.env.OPENAI_ORGANIZATION || '';
+
+export const AZURE_DEPLOYMENT_ID =
+  process.env.AZURE_DEPLOYMENT_ID || '';
