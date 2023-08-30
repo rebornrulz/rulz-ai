f0fc45644cb8978de763241d2153ae07df6f800a
Reborn-Rulz <r3b0rnrulz@gmail.com>
2023-08-30 18:13:04 +0800

    commit

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -379,7 +379,7 @@ export const ChatInput = ({
           )}
         </div>
       </div>
-      <div className="px-3 pt-2 pb-3 text-center text-[8px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
+      <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
           href="https://github.com/rebornrulz/rulz-ai"
           target="_blank"
