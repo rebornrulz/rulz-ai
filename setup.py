1101259a1be576e2c2bf0ca5748603b62bb14933
Reborn-Rulz <r3b0rnrulz@gmail.com>
2023-09-05 20:54:30 +0800

    commit

--- setup.py
+++ setup.py
@@ -14,5 +14,10 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'rasa-x',
     ],
 )
