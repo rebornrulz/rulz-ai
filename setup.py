40266e3dc1d0856bec07d7d9c59eebb31a6676bc
Reborn-Rulz <r3b0rnrulz@gmail.com>
2023-09-05 20:41:44 +0800

    commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rulz_ai',
+    version='3.0.0',
+    author='Reborn Rulz',
+    author_email='r3b0rnrulz@email.com',
+    description='Rulz-AI is a Large Language Model trained in vast data.',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
