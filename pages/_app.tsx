3bbf1c4839a388fcb0afc21ec9d422481f1c7dbf
Reborn-Rulz <r3b0rnrulz@gmail.com>
2023-09-13 07:00:54 +0800

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,56 @@
+import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { appWithTranslation } from 'next-i18next';
-import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
-
-import '@/styles/globals.css';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-function App({ Component, pageProps }: AppProps<{}>) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const router = useRouter();
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabaseClient.auth.onAuthStateChange((event, session) => {
+      switch (event) {
+        case 'SIGNED_IN':
+          router.push('/');
+          return;
+        case 'SIGNED_OUT':
+          router.push('/login');
+          return;
+      }
+    });
+    return subscription.unsubscribe;
+  }, []);
 
   return (
     <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <Analytics />
+        <SessionContextProvider
+          supabaseClient={supabaseClient}
+          initialSession={pageProps.initialSession}
+        >
+          <Component {...pageProps} />
+          <Analytics />
+        </SessionContextProvider>
       </QueryClientProvider>
     </div>
   );
 }
 
-export default appWithTranslation(App);
+export default appWithTranslation(MyApp);
