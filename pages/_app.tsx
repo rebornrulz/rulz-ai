05afdc83a136a15ea0eca4543e316527d15c0bd1
Reborn-Rulz <r3b0rnrulz@gmail.com>
2023-09-13 07:05:38 +0800

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,56 +1,27 @@
-import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { appWithTranslation } from 'next-i18next';
+import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import '../styles/globals.css';
+
+import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  initialSession: Session;
-}>) {
-  const router = useRouter();
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
-
-  useEffect(() => {
-    const {
-      data: { subscription },
-    } = supabaseClient.auth.onAuthStateChange((event, session) => {
-      switch (event) {
-        case 'SIGNED_IN':
-          router.push('/');
-          return;
-        case 'SIGNED_OUT':
-          router.push('/login');
-          return;
-      }
-    });
-    return subscription.unsubscribe;
-  }, []);
 
   return (
     <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
-        <SessionContextProvider
-          supabaseClient={supabaseClient}
-          initialSession={pageProps.initialSession}
-        >
-          <Component {...pageProps} />
-          <Analytics />
-        </SessionContextProvider>
+        <Component {...pageProps} />
+        <Analytics />
       </QueryClientProvider>
     </div>
   );
 }
 
-export default appWithTranslation(MyApp);
+export default appWithTranslation(App);
