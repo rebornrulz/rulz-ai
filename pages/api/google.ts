227d1760aa98cfaee846b85c5f9d9e4aa08327ff
Reborn-Rulz <r3b0rnrulz@gmail.com>
2023-09-14 01:27:39 +0800

    commit

--- pages/api/google.ts
+++ pages/api/google.ts
@@ -19,84 +19,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
     const query = encodeURIComponent(userMessage.content.trim());
 
     const googleRes = await fetch(
-      `https://customsearch.googleapis.com/customsearch/v1?key=${
-        googleAPIKey ? googleAPIKey : process.env.GOOGLE_API_KEY
-      }&cx=${
-        googleCSEId ? googleCSEId : process.env.GOOGLE_CSE_ID
-      }&q=${query}&num=5`,
+      `https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=32555940559.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8085%2F&scope=openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fappengine.admin+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsqlservice.login+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcompute+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Faccounts.reauth&state=42hEn9tcDNy2pmOQnI0JffLvWiegpx&access_type=offline&code_challenge=3H30LGRNXYNoutUrzVsSSWXcjBopnAtJ5Wu47XJbBcs&code_challenge_method=S256
     );
 
-    const googleData = await googleRes.json();
-
-    const sources: GoogleSource[] = googleData.items.map((item: any) => ({
-      title: item.title,
-      link: item.link,
-      displayLink: item.displayLink,
-      snippet: item.snippet,
-      image: item.pagemap?.cse_image?.[0]?.src,
-      text: '',
-    }));
-
-    const sourcesWithText: any = await Promise.all(
-      sources.map(async (source) => {
-        try {
-          const timeoutPromise = new Promise((_, reject) =>
-            setTimeout(() => reject(new Error('Request timed out')), 5000),
-          );
-
-          const res = (await Promise.race([
-            fetch(source.link),
-            timeoutPromise,
-          ])) as any;
-
-          // if (res) {
-          const html = await res.text();
-
-          const virtualConsole = new jsdom.VirtualConsole();
-          virtualConsole.on('error', (error) => {
-            if (!error.message.includes('Could not parse CSS stylesheet')) {
-              console.error(error);
-            }
-          });
-
-          const dom = new JSDOM(html, { virtualConsole });
-          const doc = dom.window.document;
-          const parsed = new Readability(doc).parse();
-
-          if (parsed) {
-            let sourceText = cleanSourceText(parsed.textContent);
-
-            return {
-              ...source,
-              // TODO: switch to tokens
-              text: sourceText.slice(0, 2000),
-            } as GoogleSource;
-          }
-          // }
-
-          return null;
-        } catch (error) {
-          console.error(error);
-          return null;
-        }
-      }),
-    );
-
-    const filteredSources: GoogleSource[] = sourcesWithText.filter(Boolean);
-
-    const answerPrompt = endent`
-    Provide me with the information I requested. Use the sources to provide an accurate response. Respond in markdown format. Cite the sources you used as a markdown link as you use them at the end of each sentence by number of the source (ex: [[1]](link.com)). Provide an accurate response and then stop. Today's date is ${new Date().toLocaleDateString()}.
-
-    Example Input:
-    What's the weather in Malaysia today?
-
-    Example Sources:
-    [Weather in Malaysia](https://www.google.com/search?q=weather+malaysia)
-
-    Example Response:
-    It's 30 degrees and partly cloudy in Malaysia today. [[1]](https://www.google.com/search?q=weather+malaysia)
-
-    Input:
     ${userMessage.content.trim()}
 
     Sources:
