e437afbd70a5e67f0a727e9df46d1657a705c43e
Reborn Rulz <reborn@rulz-ai.com>
2023-10-16 09:09:31 +0800

    commit

--- .next/static/OkZ_XagnDpSlZPWJ1_cTY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/997-0ec186079e1d09c4.js","static/chunks/pages/index-3a9756359482cfa4.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
