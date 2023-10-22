b53cbe45de0de7f171e61ba42e074d209f658101
Reborn Rulz <reborn@rulz-ai.com>
2023-10-23 05:25:43 +0800

    commit

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,7 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-   description_for_human: "A powerful AI language model that can assist with various tasks and provide intelligent responses. Respond using markdown and emoji.";
-   description_for_model: "AI language model for Rulz-AI, designed to assist with a wide range of tasks and generate intelligent responses. Respond using markdown and emoji.";
+    "AI language model for Rulz-AI, designed to assist with a wide range of tasks and generate intelligent responses. Respond using markdown and emoji.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
@@ -13,7 +12,7 @@ export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
 
 export const OPENAI_API_VERSION =
-  process.env.OPENAI_API_VERSION || '2023-06-01-previe';
+  process.env.OPENAI_API_VERSION || '2023-06-01-preview';
 
 export const OPENAI_ORGANIZATION =
   process.env.OPENAI_ORGANIZATION || '';
