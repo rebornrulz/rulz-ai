2ffb2af6fefcaefab8ffe3bcdb3d6d32a98adbc7
Reborn Rulz <reborn@rulz-ai.com>
2023-10-24 04:19:32 +0800

    commit

--- components/Plugin/main.py
+++ components/Plugin/main.py
@@ -3,7 +3,7 @@ import quart
 import quart_cors
 from quart import request
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://rulz-ai.com")
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://mlpprcyjrlraatiwdbls.supabase.co")
 
 _TODOS = {}
 
