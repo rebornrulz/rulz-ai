fc064423f0c834d8129f4c6d3ffc88712ea01224
Reborn Rulz <reborn@rulz-ai.com>
2023-10-31 00:06:29 +0800

    commit

---
+++ __checks__/https-rulz-ai-com.check.ts
@@ -0,0 +1,28 @@
+/**
+* This is a Checkly CLI BrowserCheck construct. To learn more, visit:
+* - https://www.checklyhq.com/docs/cli/
+* - https://www.checklyhq.com/docs/cli/constructs-reference/#browsercheck
+*/
+
+import { BrowserCheck, Frequency, RetryStrategyBuilder } from 'checkly/constructs'
+
+new BrowserCheck('https-rulz-ai-com', {
+  name: 'https://rulz-ai.com',
+  activated: true,
+  muted: false,
+  shouldFail: false,
+  locations: ['us-east-1'],
+  tags: [],
+  sslCheckDomain: '',
+  frequency: Frequency.EVERY_10M,
+  environmentVariables: [],
+  code: {
+    entrypoint: './https-rulz-ai-com.spec.ts',
+  },
+  retryStrategy: RetryStrategyBuilder.linearStrategy({
+    baseBackoffSeconds: 60,
+    maxRetries: 2,
+    maxDurationSeconds: 600,
+    sameRegion: true,
+  }),
+})
