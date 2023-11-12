bf28a680dc2056a1f60e6147c54c53f66c3fceae
Reborn <reborn@rulz-ai.com>
2023-11-13 07:58:26 +0800

    commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.get('/', async (req, res) => {
+    let { data, error } = await supabase
+        .from('your-table')
+        .select('*')
+
+    if (error) return res.status(500).send({ error: error.message });
+    return res.status(200).send(data);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
