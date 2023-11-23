b292eba327e0c2fee986acb7f47d4120ff1f6217
Reborn <reborn@rulz-ai.com>
2023-11-24 07:24:58 +0800

    commit

---
+++ g4f/testing/date_bot.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+
+def get_current_date():
+    today = date.today()
+    return today.strftime("%B %d, %Y")
+
+def get_date_by_input(input_date):
+    try:
+        date_obj = datetime.strptime(input_date, "%Y-%m-%d").date()
+        return date_obj.strftime("%B %d, %Y")
+    except ValueError:
+        return "Invalid date format. Please provide the date in the format YYYY-MM-DD."
+
+def chatbot_logic(user_input):
+    user_input_lower = user_input.lower()
+    
+    if "date" in user_input_lower:
+        return f"The current date is: {get_current_date()}"
+    elif "calendar" in user_input_lower:
+        # Extract the date from the user input (assuming YYYY-MM-DD format)
+        date_query = user_input_lower.replace("calendar", "").strip()
+        return get_date_by_input(date_query)
+    else:
+        return "I'm not sure what you're asking. Ask me about the date or calendar!"
