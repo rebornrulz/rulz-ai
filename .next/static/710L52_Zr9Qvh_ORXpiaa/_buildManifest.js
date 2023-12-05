7275a410e968dd29781abd2f0e74d9b9e51c84cd
Reborn <reborn@rulz-ai.com>
2023-12-05 22:07:35 +0800

    commit

--- .next/static/710L52_Zr9Qvh_ORXpiaa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/997-73c154e1266caaad.js","static/chunks/pages/index-06776ce9807cc838.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
