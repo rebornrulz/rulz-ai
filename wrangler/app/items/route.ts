45578ed344be723f5b76035e5d16fb4aa79328d9
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2024-01-12 13:43:51 +0800

    Create route.ts
    Signed-off-by: Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>

---
+++ wrangler/app/items/route.ts
@@ -0,0 +1,6 @@
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
