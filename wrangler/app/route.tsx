e37b02a55e7bdedf54e14d469c6b71749ea722ea
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2024-01-12 13:49:21 +0800

    Create route.tsx
    Signed-off-by: Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>

---
+++ wrangler/app/route.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
+export const maxDuration = 5
+ 
+export default function MyComponent() {}
