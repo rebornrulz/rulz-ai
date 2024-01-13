d1726b3b71a25eacac622119cf6d4bbfad0764ec
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2024-01-14 06:50:20 +0800

    Update langchain-google-genai.py
    Signed-off-by: Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>

--- src/peft/langchain-google-genai.py
+++ src/peft/langchain-google-genai.py
@@ -10,10 +10,3 @@ print(
         "What are some of the pros and cons of Python as a programming language?"
     )
 )
-
-llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
-print(
-    llm.invoke(
-        "What are some of the pros and cons of Python as a programming language?"
-    )
-)
