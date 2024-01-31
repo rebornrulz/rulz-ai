b72974b2b6171a39054253c428c99b97e7194803
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2024-02-01 02:33:10 +0800

    Create .eslintrc.cjs
    Signed-off-by: Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    curly: [1, 'all'],
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-unused-vars': [0, { args: 'after-used', vars: 'local' }],
+    'no-constant-condition': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
+};
