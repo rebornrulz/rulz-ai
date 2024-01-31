8ffa8337ba5491efd757e17597b3c4f32cc37984
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2024-02-01 01:33:58 +0800

    Update openai.ts
    Signed-off-by: Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>

--- types/openai.ts
+++ types/openai.ts
@@ -19,9 +19,9 @@ export enum OpenAIModelID {
   GPT_3_5_0301 = 'gpt-3.5-turbo-0301',
   GPT_3_5_1106 ='gpt-3.5-turbo-1106',
   GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
   GPT_4_0613 = 'gpt-4-0613',
-  GPT_4_32K_0613 = 'gpt-4-32k-0613',
+  GPT_4_32K = 'gpt-4-32k-0613',
+  GPT_4_0125 = 'gpt-4-turbo-preview',
   GPT_4_1106 = 'gpt-4-1106-preview',
   GPT_4V = 'gpt-4-vision-preview'
 }
@@ -82,36 +82,36 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
     maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
+    tokenLimit: 8912,
   },
   [OpenAIModelID.GPT_4_0613]: {
     id: OpenAIModelID.GPT_4_0613,
     name: 'GPT-4-0613',
     maxLength: 24000,
-    tokenLimit: 8000,
+    tokenLimit: 8192,
   },
-  [OpenAIModelID.GPT_4_32K_0613]: {
-    id: OpenAIModelID.GPT_4_32K_0613,
+  [OpenAIModelID.GPT_4_32K]: {
+    id: OpenAIModelID.GPT_4_32K,
     name: 'GPT-4-32K-0613',
     maxLength: 96000,
-    tokenLimit: 32000,
+    tokenLimit: 32768,
   },
   [OpenAIModelID.GPT_4_1106]: {
     id: OpenAIModelID.GPT_4_1106,
     name: 'GPT-4-1106-preview',
-    maxLength: 24000,
-    tokenLimit: 8000,
+    maxLength: 96000,
+    tokenLimit: 128000,
   },
   [OpenAIModelID.GPT_4V]: {
     id: OpenAIModelID.GPT_4V,
     name: 'GPT-4-vision-preview',
-    maxLength: 24000,
-    tokenLimit: 8000,
+    maxLength: 96000,
+    tokenLimit: 128000,
+  },
+  [OpenAIModelID.GPT_4_0125]: {
+    id: OpenAIModelID.GPT_4_0125,
+    name: 'GPT-4-turbo-preview',
+    maxLength: 96000,
+    tokenLimit: 128000,
   },
 };
