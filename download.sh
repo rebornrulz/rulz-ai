fc5d1027a836083d5c9d85d7edfe3633e16cb0b2
Reborn Rulz <reborn@rulz-ai.com>
2024-02-04 15:21:28 +0800

    ommit

--- download.sh
+++ download.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
