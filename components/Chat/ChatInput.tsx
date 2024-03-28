1ecfa9c138f2907770233b174c8cef51406ac32a
Reborn Rulz <reborn@rulz-ai.com>
2024-03-28 10:37:37 +0800

    commit

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -381,7 +381,7 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://www.linkedin.com/in/rulz-ai/"
+          href="https://huggingface.co/rebornrulz/Rulz-AI"
           target="_blank"
           rel="noreferrer"
           className="underline"
