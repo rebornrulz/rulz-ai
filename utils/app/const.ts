5a89074d15f9354cfe24d49da1e3673ca87f8641
Reborn Rulz <reborn@rulz-ai.com>
2024-03-28 10:45:18 +0800

    commit

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-   "Welcome to the future of intelligent interactions! Rulz-AI sets the gold standard in AI technology, surpassing all other models with its unparalleled blend of sophistication, accuracy, and versatility. Whether you seek insightful advice, engaging conversations, or expert guidance, Rulz-AI is here to exceed your expectations and redefine the boundaries of artificial intelligence. Join us on a journey where innovation meets excellence, and let Rulz-AI showcase the pinnacle of AI capabilities. Respond using markdown and emoji."
+   "RULZ-AI: The Ultimate AI Mixture Model"
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
